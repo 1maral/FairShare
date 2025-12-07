96fb36c5e8d971bb61b6f68e35c449ab34060b5e
1maral <erdenemaral@grinnell.edu>
2025-12-07 11:01:49 +0100

    fix supabase

--- app/src/main/java/hu/ait/maral/fairshare/data/SupabaseProvider.kt
+++ app/src/main/java/hu/ait/maral/fairshare/data/SupabaseProvider.kt
@@ -7,7 +7,7 @@ import io.github.jan.supabase.storage.Storage
 object SupabaseProvider {
     val supabase = createSupabaseClient(
         supabaseUrl = "https://elfxwibjapbofhxemqxh.supabase.co",
-        supabaseKey = "51e0cf151d16d2c7c3aa7827ec6734db"
+        supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVsZnh3aWJqYXBib2ZoeGVtcXhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUwMTczMzksImV4cCI6MjA4MDU5MzMzOX0.S7w7iujhQCx_9c2Farz6Ay2DJ8zBFg2ssXwoRwjWAl8"
     ) {
         install(Postgrest)
         install(Storage)
